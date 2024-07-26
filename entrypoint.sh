c70506ef35c5841ea6a9967be684b5961072640a
marina.nekrassova <marina.nekrassova@starship.co>
2024-07-26 23:11:27 +0300

    Adjust search paths for protos and output dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd $1
 
 git config --global --add safe.directory '*'
 
-protoc --rust_out=rust *.proto
+protoc -I. --rust_out=rust/uploadermessages/src protos/*.proto protos/**/*.proto
 
 git diff
 git diff-index --quiet HEAD
